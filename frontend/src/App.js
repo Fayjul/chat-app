27e6a16c4649805d7c36818c038e93c08865e16f
Fayjul Islam <fayzulislam9@gmail.com>
2022-10-16 16:05:08 +0600

    set Profile picture not complite

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Avater from './pages/Avater/Avater';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<Login />} />
+        <Route path="/avater" element={<Avater />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/Avater/Avater.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import loader from '../../assets/loader.gif';
+import './Avater.css';
+import { Buffer } from 'buffer';
+import { ToastContainer, toast } from 'react-toastify';
+
+const Avater = () => {
+  const api = `https://api.multiavatar.com/4645646`;
+  const navigate = useNavigate();
+  const [avaters, setAvaters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedAvater, setSelectedAvater] = useState(undefined);
+
+  const setProfilePicture = async () => {
+    if (selectedAvater === undefined) {
+      toast.error('Please select an avatar', { position: 'bottom-center' });
+    }
+  };
+
+  const asyncLoadData = async () => {
+    const data = [];
+
+    for (let i = 0; i < 4; i++) {
+      const image = await axios.get(
+        `${api}/${Math.round(Math.random() * 1000)}`
+      );
+      const buffer = new Buffer(image.data);
+      data.push(buffer.toString('base64'));
+    }
+    setAvaters(data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    asyncLoadData();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="main-div">
+          <img src={loader} alt="loader" className="loader"></img>
+        </div>
+      ) : (
+        <div className="main-div">
+          <div className="title-container">
+            <h1>Pick an Avatar as your profile picture</h1>
+          </div>
+          <div className="avatars">
+            {avaters.map((avatar, index) => {
+              return (
+                <div>
+                  <div
+                    className={`avatar ${
+                      selectedAvater === index ? 'selected' : ''
+                    }`}
+                  >
+                    <img
+                      src={`data:image/svg+xml;base64,${avatar}`}
+                      alt="avatar"
+                      key={avatar}
+                      onClick={() => setSelectedAvater(index)}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <button onClick={setProfilePicture} className="submit-btn">
+              Set as Profile Picture
+            </button>
+          </div>
+          <ToastContainer />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Avater;
