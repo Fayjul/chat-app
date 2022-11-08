a0276a1e56e912bfe7fefb2c2095877dedacb1eb
Fayjul Islam <fayzulislam9@gmail.com>
2022-11-08 15:55:20 +0600

    Here are some issue for takeing current user.

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,5 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { allUsersRoute } from '../../utils/APIRoutes';
 
 export default function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentChats, setCurrentChats] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  const getUser = async () => {
+    if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate('/signin');
+    } else {
+      const userFromLocalStorage = await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      );
+      //console.log(userFromLocalStorage);
+      setCurrentUser(userFromLocalStorage);
+      console.log(currentUser);
+      if (currentUser.isAvatarImageSet) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+        console.log(currentUser);
+      } else {
+        navigate('/avater');
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+  const setAvaterImagefunction = async () => {
+    if (currentUser) {
+      if (currentUser.setAvaterImage) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+        console.log(currentUser);
+      } else {
+        navigate('/avater');
+      }
+    } else {
+      console.log('Nothing found haha this is from homejs');
+    }
+  };
+  /*
+  useEffect(() => {
+    setAvaterImagefunction();
+  }, []);
+*/
+  return (
+    <div className="main-div">
+      <h1 className="brand">Wellcome to our Chat App</h1>
+    </div>
+  );
 }
--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -2,3 +2,4 @@ export const host = 'http://localhost:5000';
 export const signUpRouter = `${host}/api/auth/signup`;
 export const signinRouter = `${host}/api/auth/signin`;
 export const setAvaterRouter = `${host}/api/setavatar`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
